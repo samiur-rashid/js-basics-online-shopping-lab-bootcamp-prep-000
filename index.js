20a8948869b3627db4f3f0c6203d15cb29a72e65
Samiur Rashid <samrashid@gmail.com>
2017-10-18 17:16:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,12 +24,10 @@ function viewCart() {
     var items = [];
       for (var i = 0; i < cart.length; i++) {
         for (var item in cart[i]) {
-          if (i == cart.length - 1) {
+          if (i == cart.length) {
               items.push("and " + item + " at $" + cart[i][item])
           }
-
           items.push(item + " at $" + cart[i][item])
-
       }
       console.log("In your cart, you have " + items.join(" and ") + ".");
     }
