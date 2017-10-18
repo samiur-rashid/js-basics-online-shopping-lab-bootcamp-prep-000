86f4e8cb5273537af6fa98414a634ea5cd2e264f
Samiur Rashid <samrashid@gmail.com>
2017-10-18 16:32:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,17 +42,19 @@ function total() {
 
 function removeFromCart(item) {
   var cartItem = false;
-      for (var i = 0; i < cart.length; i++) {
-        if (cart[i].hasOwnProperty(item)) {
-          cart.splice(i, 1);
-          cartItem = true;
-        }
-      }
-      if (cartItem == false) {
-        console.log("That item is not in your cart.")
-      }
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      cartItem = true;
+    }
+  }
 
-    return cart;
+  if (cartItem == false) {
+    console.log("That item is not in your cart.")
+  }
+
+  return cart;
+
 }
 
 function placeOrder(cardNumber) {
