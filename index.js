138a3028cd0d9ebb57727b93cc94fbfa06c0a1ab
Samiur Rashid <samrashid@gmail.com>
2017-10-18 17:30:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function viewCart() {
       for (var item in cart[i]) {
         items.push(item + " at $" + cart[i][item])
     }
-    console.log("In your cart, you have " + items.join(" and ") + ".");
+    console.log("In your cart, you have " + items.join(", ") + ".");
   }
 }
 }
